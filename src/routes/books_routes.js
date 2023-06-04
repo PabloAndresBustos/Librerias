90473c4479b5ac129904d28c2a3f443061aab7d5
Pablo A Bustos <pabloandresbustos@gmail.com>
2023-06-04 18:17:50 -0300

    Agregamos los comentarios al codigo

--- src/routes/books_routes.js
+++ src/routes/books_routes.js
@@ -5,9 +5,9 @@ const { userAuthenticate } = require('../middlewares/user_authentication');
 const booksRouter = express.Router();
 
 booksRouter.get('/books', booksController.takeAllBooks);
-booksRouter.post('/new_book', userAuthenticate, booksController.addBook);
-booksRouter.get('/the_book/:id', booksController.theBook);
-booksRouter.put('/edit_book/:id', userAuthenticate, booksController.modifyBook);
+booksRouter.post('/new_book',userAuthenticate, booksController.addBook);
+booksRouter.get('/the_book/:id',booksController.theBook);
+booksRouter.put('/edit_book/:id',userAuthenticate, booksController.modifyBook);
 booksRouter.delete('/remove_book/:id',userAuthenticate, booksController.removeBook);
 
 /* Ruta adicional para traer los libros dados de baja */
